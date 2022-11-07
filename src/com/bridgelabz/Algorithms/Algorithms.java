c8e0b68ee0af11bd67858b097cb517d2e9a0a449
arc-015 <chavanavinash015@gmail.com>
2022-11-07 22:57:29 +0530

    welcome to the Data structure Algorithms programs

---
+++ src/com/bridgelabz/Algorithms/Algorithms.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.Algorithms;
+
+public class Algorithms {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Data structure Programs");
+
+    }
+}
